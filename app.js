771936622f705b90ec1efa0664ce4eb590d12a57
maadcode <manueldavila.developer@gmail.com>
2020-11-21 12:18:35 -0500

    Deleting 2 lines in js of nav

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const $toggle = document.querySelector('.toggle');
 const $banner = document.querySelector('.banner');
-const $header = document.querySelector('header');
 
 $toggle.addEventListener('click', toggle);
 
 function toggle() {
     $toggle.classList.toggle('active');
     $banner.classList.toggle('active');
-    $header.classList.toggle('active');
 }
